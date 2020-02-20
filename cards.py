e87bbcabaa69fc7b77c2f3081fb0da1d30943ac7
Akhil Dasika <akhildasika@codelab-28.rockhurst.int>
2020-02-20 09:48:57 -0600

    sorted

--- cards.py
+++ cards.py
@@ -1,7 +1,11 @@
 import random
+import sys
 
 deck = []
-suits = ["Hearts", "Diamonds", "Clubs", "Spades"]
+suits = ["♠", "♥", "♦", "♣"]
+d=[]
+s=["♠","♥","♦","♣"]
+f={1:"A",11:"J",12:"Q",13:"K"}
 
 for i in range (1, 4):
     for o in range(1, 14):
@@ -32,9 +36,10 @@ for i in range (1, 4):
         elif(o == 13):
             value = "K"
         deck.append(f"[{value} {suits[i]}]")
+
     
 
-def randomize (deck, o): 
+def randomize (deck): 
     for i in range(o-1,0,-1): 
         j = random.randint(0,i+1) 
   
@@ -42,8 +47,38 @@ def randomize (deck, o):
         deck[i],deck[j] = deck[j],deck[i] 
     return deck
 
+def deal(deck):
+    d=[]
+    s=["♠","♥","♦","♣"]
+    f={1:"A",11:"J",12:"Q",13:"K"}
+    for i in range(4):
+        for j in range(1,14):
+            d.append(f"{f[j] if j in f else j}{s[i]}")
+    return d
 
+def selectionSort(s, f):
+   d=[]
+   for i in range(len(s)):
 
-print(randomize(deck, o))
+     
+       minPosition = i
 
+       for j in range(i+1, len(s)):
+           if s[minPosition] > s[j]:
+               minPosition = j
+                
+       temp = s[i]
+       s[i] = s[minPosition]
+       s[minPosition] = temp         
+       temp = s[i]
+       f[i] = f[minPosition]
+       f[minPosition] = temp
+       f[minPosition] = temp
+d.append(f"{f[j] if i in f else j}{s[j]}")   
+
+print(deal(deck))
+print("shuffled deck") 
+print(randomize(deck))
+print("sorted deck")
+print(selectionSort(s, f))
         
